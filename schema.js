42bc4802858eb31bbdd7687205b3fac8ba428a2e
leela krishna <pji.leela.krishna@kalvium.community>
2025-02-21 10:42:59 +0530

    Added MongoDB user schema

---
+++ schema.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  roles: {
+    type: [String],
+    default: ["user"], // Default role is 'user'
+  },
+  profile: {
+    firstName: { type: String },
+    lastName: { type: String },
+    age: { type: Number },
+  },
+  lastLogin: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
